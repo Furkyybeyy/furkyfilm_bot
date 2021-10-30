436feefb8d31ec8e38b62be50e314faeb799a456
Furkyybeyy <93425547+Furkyybeyy@users.noreply.github.com>
2021-10-30 12:45:36 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,63 +1,55 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/EvaMariaBot'>𝙴𝚅𝙰 𝙼𝙰𝚁𝙸𝙰</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    START_TXT = ""HELO {},
+BENIM ADIM <a href='https://t.me/Furkyfilm_bot'FURKYFILM</a>, FILM,DIZI GIBI ICERIKLER SUNARIM, BENI KANALINA EKLEMEN YETERLI 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: 𝙴𝚟𝚊 𝙼𝚊𝚛𝚒𝚊
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: 𝚃𝚎𝚊𝚖 𝙴𝚟𝚊 𝙼𝚊𝚛𝚒𝚊
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+Komutlar."""
+    ABOUT_TXT = """✯ Bot Adı:Furkyfilm
+✯ Yapımcı:Furkan
+✯ Program dili: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ Sürüm: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Bu bot Furkan'a aittir. 
+- Source -  https://github.com/Furkyybeyy/furkyfilm_bot/
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://t.me/Furqan47>FURKAN</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+1. Bot grupta admin olmalı.
+2. Filtreleri yalnız adminler ekleyebilir...
 
 <b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+• /filter - <code>gruba filtre ekle</code>
+• /filters - <code>gruptaki filtreleri listele</code>
+• /del - <code>aktif filtreyi siler</code>
+• /delall - <code>tüm filtreleri siler (yalnızca kanalın yapımcısı kullanabilir.)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+   
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+1. Furkyfilm herhangi uzantılı filmleri destekler.
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https//t.me/furkyfilm_bot)</code>
 
 <b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
+<code>[Button Text](buttonalert:Bu bir uyarı mesaji!)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains cam rip, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+1. Botu admin yap.
+ Tüm içerikleri kanalina yukleyebilirim."""
     CONNECTION_TXT = """Help: <b>Connections</b>
 
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
 
 <b>NOTE:</b>
-1. Only admins can add a connection.
+1. Bağlantıları yalniz adminler ekleyebilir.
 2. Send <code>/connect</code> for connecting me to ur PM
 
 <b>Commands and Usage:</b>
@@ -70,37 +62,37 @@ class script(object):
 these are the extra features of tessa
 
 <b>Commands and Usage:</b>
-• /id - <code>get id of a specifed user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
+• /id - <code>mesaji gonderen kisinin id'sini gosterir.</code>
+• /info  - <code>mesaji gonderen kisi hakkinda bilgi verir.</code>
+• /imdb  - <code>IMDb sitesinden arattigin filmin bilgilerini sunar.</code>
+• /search  - <code>film hakkinda bilgi alabilirsin.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
 <b>NOTE:</b>
-This module only works for my admins
+Bu kodlar yalnız adminler icindir!
 
 <b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+• /logs - <code></code>
+• /stats - <code>kaynaklardaki filmleri listeler.</code>
+• /users - <code>kaç kisi beni kullaniyor?.</code>
+• /chats - <code>kac tane kanalda varim? </code>
+• /leave  - <code>sohbetten ayrilmak icin.</code>
+• /disable  -  <code>sohbeti devre disi birakmak icin.</code>
+• /ban  - <code>herhangi birini banlamak icin.</code>
+• /unban  - <code>herhangi birinin banini kaldirmak icin.</code>
+• /channel - <code>bulundugum tum kanallar</code>
+• /broadcast - <code>mesajı tum uyelere gonderir</code>"""
+    STATUS_TXT = """★ TOPLAM DOSYALAR: <code>{}</code>
+★ TOPLAM DOSYALAR: <code>{}</code>
+★ TOPLAM SOHBETLER: <code>{}</code>
+★ KULLANILAN HAFIZA: <code>{}</code> 𝙼𝚒𝙱
+★ BOŞ ALAN: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+Grup = {}(<code>{}</code>)
+Toplam üyeler = <code>{}</code>
+Tarafından eklendi - {}
 """
-    LOG_TEXT_P = """#NewUser
+    LOG_TEXT_P = """#Yeni kişi
 ID - <code>{}</code>
-Name - {}
+İsim - {}
 """
